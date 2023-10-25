b0fa568312bdd2f488f6aae23daa213cb3ebbd9c
Mayya Markova <mayya.markova@gmail.com>
2023-10-25 13:10:50 +0300

    Add reduce algorithm demo

---
+++ 06-reduce-algorithm/math.exs
@@ -0,0 +1,12 @@
+defmodule Math do
+  def sum_list([head | tail], accumulator) do
+    sum_list(tail, head + accumulator)
+  end
+
+  def sum_list([], accumulator) do
+    accumulator
+  end
+end
+
+IO.puts(Math.sum_list([1, 2, 3], 0))
+IO.puts(Math.sum_list([1, 2, 3, 4, 5], 0))
