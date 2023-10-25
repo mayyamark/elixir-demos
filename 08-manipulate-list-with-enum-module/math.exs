b01426e1dc9beb3f982a54cbf1dd2767201463a0
Mayya Markova <mayya.markova@gmail.com>
2023-10-25 13:49:33 +0300

    Add more Enum module demos

--- 08-manipulate-list-with-enum-module/math.exs
+++ 08-manipulate-list-with-enum-module/math.exs
@@ -3,3 +3,6 @@ IO.puts(Enum.reduce([1, 2, 3], 0, &+/2))
 
 IO.inspect(Enum.map([1, 2, 3], fn(x) -> x * 2 end))
 IO.inspect(Enum.map([1, 2, 3], &(&1 * 2)))
+
+odd? = &(rem(&1, 2) != 0)
+IO.inspect(Enum.filter(1..10, odd?))
