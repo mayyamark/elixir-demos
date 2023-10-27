e5912aa000f03d4626ab54caa327b020a20ecab0
Mayya Markova <mayya.markova@gmail.com>
2023-10-27 10:12:13 +0300

    Add deriving demo

--- 26-protocols/size.exs
+++ 26-protocols/size.exs
@@ -3,6 +3,10 @@ defprotocol Size do
   def size(data)
 end
 
+defimpl Size, for: Any do
+  def size(_), do: 0
+end
+
 defimpl Size, for: BitString do
   def size(string), do: byte_size(string)
 end
@@ -19,16 +23,24 @@ defimpl Size, for: MapSet do
   def size(set), do: MapSet.size(set)
 end
 
-# implement Size for User struct
+# define User struct
 defmodule User do
   defstruct [:name, :age]
 end
 
+# implement Size for User struct
 defimpl Size, for: User do
   def size(_user), do: 2
+end
+
+# use Size for Any
+defmodule OtherUser do
+  @derive [Size]
+  defstruct [:name, :age]
 end
 
 IO.puts Size.size("foo") # string - 3
 IO.puts Size.size(%{label: "some label"}) # map - 1
 IO.puts Size.size({:ok, "hello"}) # tuple - 2
 # IO.puts Size.size(%User{name: "John", age: 27}) # User - 2
+# IO.puts Size.size(%OtherUser{name: "John", age: 27}) # OtherUser - 2
