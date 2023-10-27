6ee272288033a4e637cbf5d8f07b909c1981260b
Mayya Markova <mayya.markova@gmail.com>
2023-10-27 10:00:27 +0300

    Add protocol demos

--- 26-protocols/size.exs
+++ 26-protocols/size.exs
@@ -15,7 +15,20 @@ defimpl Size, for: Tuple do
   def size(tuple), do: tuple_size(tuple)
 end
 
+defimpl Size, for: MapSet do
+  def size(set), do: MapSet.size(set)
+end
 
-IO.puts Size.size("foo") # string
-IO.puts Size.size(%{label: "some label"}) # map
-IO.puts Size.size({:ok, "hello"}) # tuple
+# implement Size for User struct
+defmodule User do
+  defstruct [:name, :age]
+end
+
+defimpl Size, for: User do
+  def size(_user), do: 2
+end
+
+IO.puts Size.size("foo") # string - 3
+IO.puts Size.size(%{label: "some label"}) # map - 1
+IO.puts Size.size({:ok, "hello"}) # tuple - 2
+# IO.puts Size.size(%User{name: "John", age: 27}) # User - 2
