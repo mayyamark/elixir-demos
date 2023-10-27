494955030a99149b308aa2331da375b6cb993cde
Mayya Markova <mayya.markova@gmail.com>
2023-10-27 09:41:55 +0300

    Add protocol demos

---
+++ 26-protocols/utility.exs
@@ -0,0 +1,15 @@
+defprotocol Utility do
+  @spec type(t) :: String.t()
+  def type(value)
+end
+
+defimpl Utility, for: BitString do
+  def type(_value), do: "string"
+end
+
+defimpl Utility, for: Integer do
+  def type(_value), do: "integer"
+end
+
+IO.puts Utility.type("foo")
+IO.puts Utility.type(123)
